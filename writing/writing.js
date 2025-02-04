d5aaa655f00483105851f674eaa3df72b894d042
mmack1 <77807518+mmack1@users.noreply.github.com>
2025-02-03 19:47:40 -0500

    background size container

--- writing/writing.js
+++ writing/writing.js
@@ -1,39 +1,3 @@
-const words = ["Building, breaking, and fixing things...", "Trying to make sense of the digital & the human.", "Data isn’t just numbers; it’s stories waiting to be told."]; // List of words
-let wordIndex = 0;
-let currentWord = "";
-let currentCharIndex = 0;
-const typingSpeed = 100;
-const erasingSpeed = 90;
-const cycleDelay = 800;
-
-const container = document.getElementById('typing-container');
-
-function type() {
-  if (currentCharIndex < words[wordIndex].length) {
-    currentWord += words[wordIndex][currentCharIndex];
-    container.innerHTML = `<span class="typing-text">${currentWord}</span>`; // Displaying typed text
-    currentCharIndex++;
-    setTimeout(type, typingSpeed);
-  } else {
-    setTimeout(erase, cycleDelay); // Wait before starting to erase
-  }
-}
-
-function erase() {
-  if (currentCharIndex > 0) {
-    currentWord = currentWord.slice(0, -1);
-    container.innerHTML = `<span class="typing-text">${currentWord}</span>`; // Erasing text
-    currentCharIndex--;
-    setTimeout(erase, erasingSpeed);
-  } else {
-    wordIndex = (wordIndex + 1) % words.length; // Move to the next word
-    setTimeout(type, cycleDelay); // Wait before starting to type next word
-  }
-}
-
-// Initial typing sequence
-setTimeout(type, cycleDelay);
-
 // JavaScript to handle fade-in effect on scroll
 window.addEventListener('scroll', function () {
   const sections = document.querySelectorAll('.scroll-section');
