75ee207ec081a6de93fc43b1cd2a96541974cc97
mmack1 <77807518+mmack1@users.noreply.github.com>
2025-02-10 12:16:01 -0500

    displaying all cards

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -174,7 +174,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 card.classList.remove("full");
                 card.classList.remove("side");
                 const cards = document.querySelectorAll(".info-card")
-                if (cards) card.style.display = "block";
+                if (cards) cards.style.display = "block";
 
                 // Hide the 'exit' button and the '.info' element
                 const exitButton = card.querySelector(".exit");
